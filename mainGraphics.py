70ee9e02571a5c16d22a4bdae74b900915b7aea7
Bogdan Gusuleac <bgusuleac@gmail.com>
2020-03-08 17:23:58 -0700

    Vending machine graphics

---
+++ mainGraphics.py
@@ -0,0 +1,46 @@
+from utilities import *
+
+# GLOBAL FUNCTIONS
+
+
+def increment(point, x=0, y=0):
+    return Point(point.x + x, point.y + y)
+
+# CONSTANTS
+
+
+imageLineHeight = 225
+imageHeight = 154
+imageWidth = 88
+imgList = ["antivirusPRIME.png", "antivirusPRO.png", "passwordManager.png", "optimizer.png", "systemSPEEDUP.png", "phantomVPN.png"]
+
+buttonHeight = 50
+buttonWidth  = 150
+
+buttonWidthTo2 = buttonWidth / 2
+buttonHeightTo2 = buttonHeight / 2
+
+incrementer = 200
+
+# MAIN
+
+
+system = Window("Hello", 1200, 500)
+
+pLine = Point(100, imageLineHeight)
+pLine1 = Point(100, imageLineHeight + 150)
+
+for i in range(len(imgList)):
+    system.add_image(pLine, imgList[i])
+    pLine = increment(pLine, incrementer)
+    buttonName = imgList[i].split(".")[0]
+    system.add_button(increment(pLine1, -buttonWidthTo2, -buttonHeightTo2), increment(pLine1, buttonWidthTo2, buttonHeightTo2), True, buttonName)
+    pLine1 = increment(pLine1, incrementer)
+
+system.add_image(Point(100, 50), "logo1.png")
+system.add_button(Point(1025, 420), Point(1175, 470), False, "Buy")
+
+system.print_all()
+
+while True:
+    system.get_action()
---
+++ utilities.py
@@ -0,0 +1,93 @@
+from graphics import *
+
+
+def idle():
+    return None
+
+
+class Button:
+    def __init__(self, p1, p2, showCounter, text=""):
+
+        self.name = "button"
+        self.counter = 0
+        self.showCounter = showCounter
+
+        self.p1 = p1
+        self.p2 = p2
+        self.anchor = Point((p1.x + p2.x) / 2, (p1.y + p2.y) / 2)
+        self.rect = Rectangle(p1, p2)
+        self.text = Text(self.anchor, text)
+
+        textCounterPos = Point(self.anchor.x, self.anchor.y - (self.p1.y / 2) - 80)
+        self.textCounter = Text(textCounterPos, "Quantity: " + str(self.counter))
+
+    def print_obj(self, win):
+
+        self.rect.draw(win)
+        self.text.draw(win)
+
+        if self.showCounter is True:
+            self.textCounter.draw(win)
+
+    def is_pressed(self, p):
+        return self.p1.x < p.x < self.p2.x and self.p1.y < p.y < self.p2.y
+
+
+    def increase_quantity(self, win):
+        self.counter += 1
+        self.textCounter.undraw()
+        self.textCounter.setText("Quantity: " + str(self.counter))
+        self.textCounter.draw(win)
+
+
+class ImageHolder:
+    def __init__(self, p, name):
+
+        self.name = "image"
+        self.img = Image(p, name)
+        self.width = self.img.getWidth()
+        self.height = self.img.getHeight()
+        anchor = self.img.getAnchor()
+
+        self.p1 = Point(anchor.getX() - (self.width / 2), anchor.getY() - (self.height / 2))
+        self.p2 = Point(anchor.getX() + (self.width / 2), anchor.getY() + (self.height / 2))
+
+        self.rect = Rectangle(self.p1, self.p2)
+
+    def is_pressed(self, p):
+        return self.p1.x < p.x < self.p2.x and self.p1.y < p.y < self.p2.y
+
+    def print_obj(self, win):
+        self.img.draw(win)
+
+class Window:
+    def __init__(self, name, width, height):
+        self.width = width
+        self.height = height
+        self.win = GraphWin(name, self.width, self.height)
+        self.addedObjects = []
+
+    def add_button(self, p1, p2, id, text=""):
+        obj = Button(p1, p2, id, text)
+        self.addedObjects.append(obj)
+
+    def add_image(self, anchor, name):
+        obj = ImageHolder(anchor, name)
+        self.addedObjects.append(obj)
+
+    def add_entry(self, anchor, width):
+        obj = Entry(anchor, width)
+        self.addedObjects.append(obj)
+
+    def get_action(self):
+        mouse = self.win.checkMouse()
+        if mouse is not None:
+            for obj in self.addedObjects:
+                if obj.name == "button":
+                    #print(obj.is_pressed(mouse))
+                    if obj.is_pressed(mouse):
+                        obj.increase_quantity(self.win)
+
+    def print_all(self):
+        for obj in self.addedObjects:
+            obj.print_obj(self.win)
